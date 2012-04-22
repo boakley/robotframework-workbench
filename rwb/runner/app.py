6ab6b2cadf11ce99e05b7f794e8bc62e4e0a40c7
Bryan Oakley <bryan.oakley@gmail.com>
2012-04-22 13:09:02 -0500

    refactord to use controller and log, tree and console views
    The logic for running a test and polling its output has been
    moved to a controller. In addition, I cleaned up the API between
    the controller and these views; they now use an interface almost
    identical to robot's own listener interface. The main difference
    being, I pass an event ID to each listener message.

--- rwb/runner/app.py
+++ rwb/runner/app.py
@@ -28,27 +28,29 @@ import ttk
 import tkFont
 import sys
 import os
-from robot_console import RobotConsole
-from robot_log import RobotLog
-from robot_messages import RobotMessages
+from controller import RobotController
+from console import RobotConsole
+from log import RobotLogTree, RobotLogMessages
 from listener import RemoteRobotListener
 from robot_tally import RobotTally
 from rwb.lib import AbstractRwbApp
+from settings import GeneralSettingsFrame
 from tsubprocess import Process
 import shlex
+import rwb
 
 NAME="runner"
 
 DEFAULT_SETTINGS = {
     NAME: {
-        "pybot": "python -m robot.runner",
+        "pybot": "python -m robot.runner '%S'",
         }
     }
 
 class RunnerApp(AbstractRwbApp):
     def __init__(self):
         # Among other things, this constructor initializes
-        # logging snd preferences. 
+        # logging and preferences. 
         AbstractRwbApp.__init__(self, NAME, DEFAULT_SETTINGS)
         self.wm_geometry("800x600")
         self.tally = RobotTally()
@@ -57,22 +59,17 @@ class RunnerApp(AbstractRwbApp):
         self._create_statusbar()
         self._create_toolbar()
         self._create_notebook()
+
+        self.register(GeneralSettingsFrame)
         
-        # every event gets an id, a simple incrementing integer. The plan
-        # is that each display (console, log, etc) will be able to jump
-        # to the related item in some other display using this id (ie:
-        # from the message window you can jump to the details window or
-        # visa versa) (this isn't implemented yet...)
-        self._id = 0
+        self._controller = RobotController(self)
+        self._controller.configure(listeners=(self.console,self.log_tree,
+                                              self.log_messages, self.tally,
+                                              self))
 
-        self._listener = RemoteRobotListener(self, self._listen)
-        self._port = self._listener.port
-#        label = ttk.Label(self, text="port: %s" % self._port)
-#        label.pack(side="bottom", fill="x")
-
-        self._poll_job_id = None
-        self.process = None
-        self.after(1, self.start_test)
+    def status_message(self, message):
+        '''Print a message in the statusbar'''
+        self.status_label.configure(text=message)
 
     def _create_statusbar(self):
         self.statusbar = ttk.Frame(self)
@@ -82,12 +79,13 @@ class RunnerApp(AbstractRwbApp):
         self.status_label.pack(side="left", fill="both", expand="true", padx=8)
         self.statusbar.pack(side="bottom", fill="x")
 
-    def status_message(self, message):
-        self.status_label.configure(text=message)
-
     def _create_menubar(self):
         self.menubar = tk.Menu(self)
         self.configure(menu=self.menubar)
+
+        self.rwbMenu = tk.Menu(self, tearoff=False)
+        self.menubar.add_cascade(menu=self.rwbMenu, label="rwb", underline=0)
+        self.rwbMenu.add_command(label="Settings", command=self.show_settings_dialog)
 
         self.file_menu = tk.Menu(self.menubar, tearoff=False)
         self.file_menu.add_command(label="Exit", command=self._on_exit)
@@ -156,33 +154,21 @@ class RunnerApp(AbstractRwbApp):
         self.stop_button.configure(state="disabled")
         self.toolbar.grid_columnconfigure(2, weight=2)
         self.value = value
+        self.tally["critical","fail"].trace_variable("w", self._on_tally_trace)
 
-    def add_result(self, result):
-        print "result:", result
-
-    def poll(self, delay):
-        if self.process:
-            (stdout, stderr) = self.process.read()
-            self.robot_console.append(stderr, "stderr")
-            self.robot_console.append(stdout, "stdout")
-
-            # see if it's still alive
-            if self.process.exit_code() is not None:
-                # It's dead, Jim.
-                self.process = None
-                self.stop_button.configure(state="disabled")
-                self.start_button.configure(state="normal")
-
-        self._poll_job_id = self.after(delay, self.poll, delay)
+    def _on_tally_trace(self, name1, name2, mode):
+        if self.tally["critical","fail"].get() > 0:
+            self.value["critical","fail"].configure(foreground="red")
+            self.value["all","fail"].configure(foreground="red")
 
     def _create_notebook(self):
         self.notebook = ttk.Notebook(self)
-        self.robot_console = RobotConsole(self.notebook)
-        self.robot_log = RobotLog(self.notebook)
-        self.robot_messages = RobotMessages(self.notebook)
-        self.notebook.add(self.robot_log, text="Details")
-        self.notebook.add(self.robot_console, text="Console")
-        self.notebook.add(self.robot_messages, text="Messages")
+        self.console = RobotConsole(self.notebook)
+        self.log_tree = RobotLogTree(self.notebook)
+        self.log_messages = RobotLogMessages(self.notebook)
+        self.notebook.add(self.log_tree, text="Details")
+        self.notebook.add(self.console, text="Console")
+        self.notebook.add(self.log_messages, text="Messages")
         self.notebook.pack(side="top", fill="both", expand=True)
 
     def _create_fonts(self):
@@ -196,72 +182,35 @@ class RunnerApp(AbstractRwbApp):
         # Throughout the app, fonts will typically be referenced
         # by their name so we don't have to pass references around.
         # Tk named fonts are a wonderful thing. 
-        print "creating fonts..."
         base_size = self.fonts.default.cget("size")
         self.fonts.big_font = self.fonts.clone_font(self.fonts.default, "big_font", 
                                                     size=int(base_size*2.0))
         self.fonts.medium_font = self.fonts.clone_font(self.fonts.default, "medium_font", 
                                                        size=int(base_size*1.5))
 
-    def _listen(self, name, *args):
-        self._id += 1
-        self.robot_log.add(self._id, name, *args)
-        if name == "log_message":
-            self.robot_messages.add(self._id, *args)
-        self.update_statusbar(name, args)
+    def reset(self):
+        self.status_message("")
 
-        # bleh. I hate how I implemented this. 
-        if name == "end_test":
-            (name, attrs) = args
-            self.tally.add_result(attrs)
-            if self.tally.get("critical","fail") > 0:
-                self.value["critical","fail"].configure(foreground="red")
-
-    def update_statusbar(self, event_name, args):
+    def listen(self, event_id, event_name, args):
         '''Update the statusbar based on the passed-in event'''
         if event_name in ("start_suite", "start_test"):
             (name, attrs) = args
             self.status_message(attrs["longname"])
-        elif event_name in ("end_suite", "end_test"):
-            self.status_message("")
         elif event_name == "close":
             self.status_message(str(self.tally))
 
     def start_test(self):
-        self.robot_log.reset()
-        self.robot_console.reset()
-        self.robot_messages.reset()
+        self._controller.configure(args=sys.argv[1:])
         self.stop_button.configure(state="normal")
         self.start_button.configure(state="disabled")
-        here = os.path.dirname(__file__)
-        listener = os.path.join(here, "socket_listener.py:%s" % self._port)
-        args = ["--listener", listener, "--log", "NONE", "--report", "NONE"]
-        files = sys.argv[1:]
-        # if user used option --runner 'blah', use that; otherwise,
-        # use "python -m robot.runner". 
-
-        cmdstring = self.get_setting("runner.pybot", "python -m robot.runner")
-#        cmdstring = "python -m robot.runner"
-        cmd = shlex.split(cmdstring) + args + files
-        self.log.debug("command:" + " ".join(cmd))
-        self.robot_log.add(self._id, "start_process", cmd)
-        self.process = Process(cmd)
-        if self._poll_job_id is not None:
-            self.after_cancel(self._poll_job_id)
-            self._poll_job_id = None
-        self.poll(100)
+        self._controller.start()
 
     def stop_test(self):
-        if self.process:
-            self.process.terminate()
-
+        controller.stop()
         self.stop_button.configure(state="disabled")
         self.start_button.configure(state="normal")
 
     def _on_exit(self):
-        self._poll_job_id
-        if self._poll_job_id:
-            self.after_cancel(self._poll_job_id)
         self.destroy()
         sys.exit(0)
 

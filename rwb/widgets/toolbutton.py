a5009ca5da5f077600e15435d424bfe574ed7324
Bryan Oakley <bryan.oakley@gmail.com>
2012-05-06 19:08:13 -0500

    added support for text-only buttons

--- rwb/widgets/toolbutton.py
+++ rwb/widgets/toolbutton.py
@@ -2,8 +2,16 @@ import Tkinter as tk
 import ttk
 
 class ToolButton(ttk.Button):
-    def __init__(self, parent, imagedata, tooltip="the tooltip", text=None,command=None):
-        self._normal_image = tk.PhotoImage(data=imagedata)
+    '''A button designed to be placed on a toolbar
+
+    This has the behavior of looking flat until you hover over it. It
+    also supports tooltips.
+    '''
+    def __init__(self, parent, imagedata=None, tooltip="the tooltip", text=None,command=None, width=5):
+        if imagedata is None:
+            self._normal_image = None
+        else:
+            self._normal_image = tk.PhotoImage(data=imagedata)
 
         self.tooltip = tooltip
         # TODO: add support for a disabled image, as well as centered stuff
@@ -12,7 +20,7 @@ class ToolButton(ttk.Button):
                             command=command, takefocus=False)
         if text is not None:
             # FIXME: this is the wrong place to set the width...
-            self.configure(text=text, compound="top", width=5)
+            self.configure(text=text, compound="top", width=width)
         self.bind("<Enter>", self._on_show_tip)
         self.bind("<Leave>", self._on_hide_tip)
         self.tip = tk.Toplevel(self, borderwidth=1, relief="solid")

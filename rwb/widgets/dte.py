fbe1b1d5c01661ebadbb2ac83e13fb356c6e24c2
Bryan Oakley <bryan.oakley@gmail.com>
2012-05-06 19:04:42 -0500

    removed unused decorator

--- rwb/widgets/dte.py
+++ rwb/widgets/dte.py
@@ -23,17 +23,6 @@ print tk.__file__
 
 '''
 
-# decorator which can be added to a method
-def timeit(func):
-    import time
-    def wrapper(*arg):
-        t1 = time.time()
-        res = func(*arg)
-        t2 = time.time()
-        print '%s took %0.3f ms' % (func.func_name, (t2-t1)*1000.0)
-        import sys; sys.stdout.flush()
-        return res
-    return wrapper
 
 class QuietIntVar(tk.IntVar):
     '''

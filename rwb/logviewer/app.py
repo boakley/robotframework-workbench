dc62b96d4e59694d780ae7f8b0c72c5c4ebfe65e
Bryan Oakley <bryan.oakley@gmail.com>
2012-05-21 18:48:59 -0500

    added some simple argument parsing and some bug fixes

--- rwb/logviewer/app.py
+++ rwb/logviewer/app.py
@@ -7,7 +7,8 @@ from rwb.widgets import Statusbar, ToolButton
 from rwb.lib.configobj import ConfigObj
 from logviewer import LogTree
 import tkFileDialog
-
+import tkMessageBox
+import argparse
 
 NAME="logviewer"
 
@@ -17,20 +18,49 @@ DEFAULT_SETTINGS = {
 
 class LogViewerApp(AbstractRwbApp):
     def __init__(self):
+        self.args = self._parse_args()
+
         AbstractRwbApp.__init__(self, NAME, DEFAULT_SETTINGS)
-        
+
         self._create_menubar()
         self._create_toolbar()
         self._create_statusbar()
 
         vsb = ttk.Scrollbar(self, orient="vertical")
-        self.viewer = LogTree(self)
+        self.viewer = LogTree(self, fail_only=self.args.fail_only)
         vsb.configure(command=self.viewer.tree.yview)
         self.viewer.tree.configure(yscrollcommand=vsb.set)
         self.toolbar.pack(side="top", fill="x")
         self.statusbar.pack(side="bottom", fill="x")
         vsb.pack(side="right", fill="y")
         self.viewer.pack(side="top", fill="both", expand=True)
+        self.viewer.tree.bind("<<TreeviewSelect>>", self._on_select)
+        
+        if self.args.file != None:
+            self.after_idle(self.after, 1, lambda path=self.args.file: self.open(path))
+#            self.after_idle(self.after, 1, lambda path=sys.argv[1]: self.open(path))
+
+        self.wm_geometry("1000x800")
+
+    def _parse_args(self):
+        '''Parse command line arguments'''
+        parser = argparse.ArgumentParser(prog="rwb.logviewer")
+        parser.add_argument("file",nargs="?",
+                            help="the path to a robot output file (eg: output.xml)")
+        parser.add_argument("-f", "--fail_only", action="store_true",
+                            help="only display failed suites, tests and keywords")
+        return parser.parse_args()
+
+    def _on_select(self, event):
+        print "select!"
+        selection = self.viewer.tree.selection()
+        print "selection:", selection
+        if len(selection) > 0:
+            item = selection[0]
+            print "item:", item
+            print "tags:", self.viewer.tree.item(item, "tags")
+            print "values:", self.viewer.tree.item(item, "values")
+            x = self.viewer.tree.item(item, "values")[-1]
 
     def _create_toolbar(self):
         self.toolbar = ttk.Frame(self)
@@ -40,13 +70,51 @@ class LogViewerApp(AbstractRwbApp):
                                  tooltip="Expand to show all suites and test cases")
         kw_button = ToolButton(self.toolbar, text="Test Keywords",  width=0,
                                tooltip="Expand to show all suites, test cases, and their keywords")
-        all_button = ToolButton(self.toolbar, text="All Keywords", width=0,
+        all_button = ToolButton(self.toolbar, text="Expand All Keywords", width=0,
                                 tooltip="Expand to show all suites, test cases, and keywords")
+
+        # this is just for experimentation; I don't really plan to have this on the toolbar
+        next_fail = ToolButton(self.toolbar, text="Next Failure", width=0,
+                               tooltip="Go to next failed keyword", command=self._on_next_fail)
+        prev_fail = ToolButton(self.toolbar, text="Previous Failure", width=0,
+                               tooltip="Go to previous failed keyword", command=self._on_previous_fail)
+
         suite_button.pack(side="left")
         test_button.pack(side="left")
         kw_button.pack(side="left")
         all_button.pack(side="left")
+        ttk.Separator(self.toolbar, orient="vertical").pack(side="left", padx=4, pady=2, fill="y")
+        next_fail.pack(side="left")
+        prev_fail.pack(side="left")
 
+    def _on_next_fail(self):
+        item = self.viewer.next_with_tag("FAIL")
+        if item is not None and item != "":
+            self.viewer.tree.see(item)
+            if self.viewer.tree.bbox(item) == "":
+                # for some reason, that previous call to .see() doesn't always
+                # scroll the item into view. If we detect that, it means it
+                # is off screen so lets call see() again, then scroll up a 
+                # few lines to show some context.
+                self.after_idle(self.viewer.tree.see, item)
+                self.after_idle(self.viewer.tree.yview_scroll,4, "units")
+            self.viewer.tree.focus(item)
+            self.viewer.tree.selection_set(item)
+
+    def _on_previous_fail(self):
+        item = self.viewer.previous_with_tag("FAIL")
+        if item is not None and item != "":
+            self.viewer.tree.see(item)
+            if self.viewer.tree.bbox(item) == "":
+                # for some reason, that previous call to .see() doesn't always
+                # scroll the item into view. If we detect that, it means it
+                # is off screen so lets call see() again, then scroll up a 
+                # few lines to show some context.
+                self.after_idle(self.viewer.tree.see, item)
+                self.after_idle(self.viewer.tree.yview_scroll,-4, "units")
+            self.viewer.tree.focus(item)
+            self.viewer.tree.selection_set(item)
+        
     def _create_menubar(self):
         self.menubar = tk.Menu(self)
         self.configure(menu=self.menubar)
@@ -59,7 +127,8 @@ class LogViewerApp(AbstractRwbApp):
         self.view_menu.add_command(label="Expand Suites", underline=7)
         self.view_menu.add_command(label="Expand Test Cases", underline=7)
         self.view_menu.add_command(label="Expand Test Case Keywords", underline=17)
-        self.view_menu.add_command(label="All Keywords", underline=0)
+        self.view_menu.add_command(label="All Keywords", underline=0, 
+                                   command=self._on_expand_all)
 
         self.menubar.add_cascade(menu=self.file_menu, label="File", underline=0)
         self.menubar.add_cascade(menu=self.view_menu, label="View", underline=0)
@@ -80,8 +149,19 @@ class LogViewerApp(AbstractRwbApp):
                                                 initialdir = initialdir,
                                                 title="Open")
         if filename is not None and filename != "":
-            self.wm_title("%s - Robot Framework Workbench Log Viewer" % filename)
-            self.viewer.refresh(filename)
+            self.open(filename)
+
+    def open(self, path):
+        try:
+            self.viewer.refresh(path)
+            self.wm_title("%s - Robot Framework Workbench Log Viewer" % path)
+        except Exception, e:
+            print "WTF?", e
+            tkMessageBox.showwarning("Unable to open the file\n\n%s" % str(e))
+            self.wm_title("Robot Framework Workbench Log Viewer")
+
+    def _on_expand_all(self):
+        self.viewer.expand_all()
 
     def _on_exit(self):
         self.destroy()

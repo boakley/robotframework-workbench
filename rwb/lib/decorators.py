c33c8a5354000bc521679e1a3b31a2095c25c29a
Bryan Oakley <bryan.oakley@gmail.com>
2012-05-21 21:13:37 -0500

    added some decorators

---
+++ rwb/lib/decorators.py
@@ -0,0 +1,55 @@
+
+def timeit(func):
+    import time
+    def _timeit(*arg):
+        t1 = time.time()
+        res = func(*arg)
+        t2 = time.time()
+        print '%s took %0.3f ms' % (func.func_name, (t2-t1)*1000.0)
+        import sys; sys.stdout.flush()
+        return res
+    return _timeit
+
+def cached_property(func):
+    '''A cached property. 
+
+    The original function will be evaluated only once, then
+    the cached result will be returned on subsequent calls
+
+    Example:
+
+    class Foo(object):
+        @cached_property
+        def message(self):
+            result = <some heavyweight calculation>
+            return result
+    '''
+    attr_name = '__cached__' + func.__name__
+    @property
+    def _cached_property(self):
+        try:
+            return getattr(self, attr_name)
+        except AttributeError:
+            setattr(self, attr_name, func(self))
+            return getattr(self, attr_name)
+    return _cached_property
+
+if __name__ == "__main__":
+    class Foo(object):
+        @cached_property
+        def foo(self):
+            print "calculating foo..."
+            return "this is foo"
+
+        @cached_property
+        def bar(self):
+            print "calculating bar..."
+            return "this is bar"
+
+
+    foo=Foo()
+    print "foo.foo:", foo.foo
+    print "foo.bar:", foo.bar
+    print "foo.foo:", foo.foo
+    print "foo.bar:", foo.bar
+

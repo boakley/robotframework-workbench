d6afff0e523c1968b56c690c3867b47015bc9796
Bryan Oakley <bryan.oakley@gmail.com>
2012-07-29 14:38:42 -0500

    total rewrite; use select rather than using tk fileevents

--- rwb/runner/listener.py
+++ rwb/runner/listener.py
@@ -8,89 +8,62 @@ for results from the SocketListener.
 
 '''
 
-import SocketServer
-import StringIO
-import Tkinter as tk
 import json
+from SimpleXMLRPCServer import SimpleXMLRPCServer, SimpleXMLRPCRequestHandler
+import socket
+import select
 
-class JSONSocketServer(tk.Frame):
-    '''A simple event-based socket server
+class JSONSocketServer(object):
+    backlog = 256
+    buffsize = 2048
+    sleep = 100 # milliseconds
+    def __init__(self, app, callback, host="localhost", port=8910):
+        self._callback = callback
+        self.port = port
+        self.app = app
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        self.server.bind((host, port)) 
+        self.server.listen(self.backlog) 
+        self.input = [self.server] 
+        self.poll()
+        self.buffer = {}
 
-    This listens for messages on the socket, translates them
-    from JSON, then calls the callback.
+    def callback(self, data):
+        deserialized_json = json.loads(data)
+        self._callback(*deserialized_json)
 
-    This uses the Tkinter socket code and fileevents because
-    they are (IMHO) much easier to work with than python threads. 
-    It is implemented as a widget so we can work with the
-    event loop, and for easy cleanup when the widget is destroyed
-    (but before the python object is actually deleted)
+    def has_clients(self):
+        return len(self.input) > 1
 
-    Inputs:
+    def poll(self):
+        inputready,outputready,exceptready = select.select(self.input,[],[],0) 
 
-    root     - a tkinter root window
-    callback - a function to be called upon receipt of a message
+        for s in inputready: 
 
-    '''
-    def __init__(self, parent, callback, port=0):
-        self._parent = parent
-        self.callback = callback
-        tk.Frame.__init__(self, parent)
-        self.bind("<Destroy>", self._on_destroy)
+            if s == self.server: 
+                # handle the server socket 
+                client, address = s.accept() 
+                self.input.append(client) 
+                self.buffer[client] = ""
 
-        # this creates a tcl command named "socket_callback" which 
-        # is a proxy for our python method named "callback_proxy"
-        parent.createcommand("socket_callback", self.callback_proxy)
+            else: 
+                # handle all other sockets 
+                data = s.recv(self.buffsize) 
+                if data: 
+                    # we received some data. Treat the socket as if it
+                    # was line buffered, by calling the callback for
+                    # each complete line we've received.
+                    self.buffer[s] += data
+                    lines = self.buffer[s].split("\n")
+                    self.buffer[s] = lines.pop()
+                    for line in lines:
+                        self.callback(line)
+                else: 
+                    s.close() 
+                    self.input.remove(s) 
+                    del self.buffer[s]
+        self.app.after(self.sleep, self.poll)
 
-        # This runs some Tcl code to establish a socket server. 
-        # the code returns the port that the server is running on.
-        # Sorry, but Tcl's socket handling with fileevents is 
-        # superior to anything python has 
-        self.port = parent.eval('''
-            proc Server {channel addr port} {
-                fileevent $channel readable [list readline $channel]
-            }
-            proc readline {channel} {
-                if {[gets $channel line] == -1} {
-                    fileevent $channel readable {}
-                    close $channel
-                } else {
-                    socket_callback $line
-                }
-            }
-            set ::the_socket [socket -server Server %s]
-            set port [lindex [fconfigure $::the_socket -sockname] end]
-        ''' % port)
-
-    def callback_proxy(self, data):
-        '''Deserialize the data and pass it to the callback
-
-        This is called by the underlying tcl socket mechanism
-        whenever data arrives on the socket.
-        '''
-        try:
-            self.callback(*json.loads(data))
-        except Exception, e:
-            print "%s.callback_proxy(): WTF?" % __file__, e
-            import traceback
-            traceback.print_exc()
-            foo = traceback.format_exc()
-            print "really, WTF?", foo
-
-
-    def _on_destroy(self, event):
-        '''Called when this object is destroyed
-
-        This stops the socket server. 
-        '''
-        self._parent.eval('''
-            # Without making a quick connection to the server, the
-            # program will hang. 
-            catch {
-                set sock [socket localhost %s]
-                # close $foo
-                close $::the_socket
-            }
-       ''' % self.port)
 
 # for backwards compatibility with older code...
 # (old. HA! this isn't even at version 1.0 yet!)
